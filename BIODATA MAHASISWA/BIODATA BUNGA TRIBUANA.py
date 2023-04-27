eba2d6c02ff37126c59ca63e14f611b9764b68ca
BUNGA TRIBUANA <129752082+Bungatribuana@users.noreply.github.com>
2023-04-27 12:44:44 +0800

    Create BIODATA BUNGA TRIBUANA.py
    biodata dan nilai mahasiswa

---
+++ BIODATA MAHASISWA/BIODATA BUNGA TRIBUANA.py
@@ -0,0 +1,27 @@
+#BIODATA MAHASISWA 
+nama_mahasiswa = input ("MASUKKAN NAMA MAHASISWA = ") 
+nim = input ("NIM = ")
+alamat = input ("ALAMAT = ")
+Jurusan = input ("JURUSAN/PRODI = ")
+Semester = input ("SEMESTER = ")
+ipk = input ("3.5 = ")
+nilai_matkul = input ( "8.5 = ")
+
+#BIODATA MAHASISWA 
+nama_mahasiswa = input ("Bunga Tribuana") 
+nim = input ("SI19220030")
+alamat = input (" Desa Lekor, Kecamatan Janapria")
+Jurusan = input ("Sistem Informasi")
+Semester = input ("Semester II")
+ipk = input ("3.5")
+nilai_matkul = input ( "8.5")
+
+#BIODATA DAN NILAI
+print ("Nama Mahasiswa = ", nama_mahasiswa)
+print ("NIM = ", nim)
+print ("Alamat = ", alamat)
+print ("Jurusan = ", jurusan)
+print ("Semester = ", semester)
+print ("IPK = ", ipk)
+print ("Nilai Mata Kuliah = ", nilai_matkul)
+
